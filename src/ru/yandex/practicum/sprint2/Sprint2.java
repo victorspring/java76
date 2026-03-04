a970f127fa6f8bffff06fe9d7cb6045049fd0a85
Victor <victorbodrovit@gmail.com>
2026-03-04 23:35:44 +0700

    Sprint2 commit

---
+++ src/ru/yandex/practicum/sprint2/Sprint2.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.sprint2;
+
+import java.util.Arrays;
+
+public class Sprint2 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3};
+        System.out.println(numbers);
+        System.out.println(Arrays.toString(numbers));
+//        Arrays a = new Arrays();
+    }
+}
